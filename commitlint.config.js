1184e062953e0c96778f7c1cf2a9003fd014164a
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-06-16 15:00:42 -0300

    feat: additional config for commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0, 'always', 100000],
+    'body-max-line-length': [0, 'always', 100000]
+  }
 }
